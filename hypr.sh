812ba151f2aec2f1d45efd7c643b607515cc8ae5
Bahaa <121813996+bahaa13v@users.noreply.github.com>
2025-03-17 15:04:27 +0100

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -4,6 +4,3 @@ git clone --recursive https://github.com/hyprwm/Hyprland
 
 cd Hyprland
 make legacyrenderer && sudo cp ./build/Hyprland /usr/bin && sudo cp ./example/hyprland.desktop /usr/share/wayland-sessions
-
-mkdir -p ~/.config/Hyprland
-cp ../Hyprlang.conf ~/.config/Hyprland/
